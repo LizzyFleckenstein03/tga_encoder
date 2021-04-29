d3f35f55ae28f18eb688ddd31cecbdb98881fa92
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-04-29 13:13:34 +0200

    Add save_image

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 tga_encoder = {}
+
+function tga_encoder.encode_pixels(pixels)
+end
+
+function tga_encoder.save_image(filename, pixels)
+	local f = assert(io.open(filename))
+	local encoded = tga_encoder.encode_pixels(pixels)
+	f:write(encoded)
+	f:close()
+end
